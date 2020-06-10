c26e5eabb2931721c68515af06eeb1e0128ae154
Taeil Kwak <taeil2@gmail.com>
2020-06-10 10:55:59 -0500

    made some formattting changes

--- src/drills.js
+++ src/drills.js
@@ -2,76 +2,76 @@ require('dotenv').config()
 const knex = require('knex');
 
 const knexInstance = knex({
-    client: 'pg',
-    connection: process.env.DB_URL,
+  client: 'pg',
+  connection: process.env.DB_URL,
 })
 
 function searchProductByName(searchTerm) {
-    knexInstance
-        .select('*')
-        .from('shopping_list')
-        .where('name', 'ILIKE', `%${searchTerm}%`)
-        .then(data => {
-            console.log(data)
-        })
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(data => {
+      console.log(data);
+    });
 }
 
 // searchProductByName('Fish')
 
 function paginateItems(pageNum) {
-    const productsPerPage = 6
-    const offset = productsPerPage * (pageNum - 1)
-    knexInstance
-        .select('*')
-        .from('shopping_list')
-        .limit(productsPerPage)
-        .offset(offset)
-        .then(data => {
-            console.log(data)
-        })
+  const productsPerPage = 6;
+  const offset = productsPerPage * (pageNum - 1);
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(data => {
+      console.log(data);
+    });
 }
 
 // paginateItems(2)
 
 function searchAfterDate(daysAgo) {
-    let date = new Date();
-    date.setDate(date.getDate() - daysAgo);
+  let date = new Date();
+  date.setDate(date.getDate() - daysAgo);
 
-    knexInstance
-        .select('*')
-        .from('shopping_list')
-        .where('date_added', '>', date)
-        .orderBy([{ column: 'date_added', order: 'DESC' }])
-        .then(data => {
-            console.log('logging date');
-            console.log(data);
-        })
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('date_added', '>', date)
+    .orderBy([{ column: 'date_added', order: 'DESC' }])
+    .then(data => {
+      console.log('logging date');
+      console.log(data);
+    });
 }
 
 // searchAfterDate(2);
 
 function getTotalCost() {
-    const categories = ['Main', 'Snack','Lunch', 'Breakfast'];
+  const categories = ['Main', 'Snack','Lunch', 'Breakfast'];
 
-    categories.forEach((category) => {
-        knexInstance
-            .select('*')
-            .from('shopping_list')
-            .where({category: category})
-            .then(data => {
-                console.log(`${category} Total: $${getTotal(data)}`);
-            });
-    });
+  categories.forEach((category) => {
+    knexInstance
+      .select('*')
+      .from('shopping_list')
+      .where({category: category})
+      .then(data => {
+        console.log(`${category} Total: $${getTotal(data)}`);
+      });
+  });
 }
 
 function getTotal(data) {
-    let total = 0;
-    data.forEach((item) => {
-        let price = parseFloat(item.price);
-        total += price;
-        total = Math.round(total * 100) / 100;
-    });
-    return total;
+  let total = 0;
+  data.forEach((item) => {
+    let price = parseFloat(item.price);
+    total += price;
+    total = Math.round(total * 100) / 100;
+  });
+  return total;
 }
 
 // getTotalCost();
--- src/practice.js
+++ src/practice.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const knex = require('knex');
 
 const knexInstance = knex({
-    client: 'pg',
-    connection: process.env.DB_URL,
-})
+  client: 'pg',
+  connection: process.env.DB_URL,
+});
 
 // const qry = knexInstance
 //     .select('product_id', 'name', 'price', 'category')
@@ -15,69 +15,63 @@ const knexInstance = knex({
 
 //  console.log(qry)
 
-
-
 function searchByProduceName(searchTerm) {
-    knexInstance
-      .select('product_id', 'name', 'price', 'category')
-      .from('amazong_products')
-      .where('name', 'ILIKE', `%${searchTerm}%`)
-      .then(result => {
-        console.log(result)
-      })
-  }
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
+}
 
 // searchByProduceName('holo')
 
 function paginateProducts(page) {
-    const productsPerPage = 10
-    const offset = productsPerPage * (page - 1)
-    knexInstance
+  const productsPerPage = 10
+  const offset = productsPerPage * (page - 1)
+  knexInstance
     .select('*')
     .from('amazong_products')
     .limit(productsPerPage)
     .offset(offset)
     .then(result => {
-        console.log(result)
-    })
+      console.log(result);
+    });
 }
 
 // paginateProducts(2)
 
-
-// function getProductsWithImages() {
-//     knexInstance
-//         .select('product_id', 'name', 'price', 'category', 'image')
-//         .from('amazong_products')
-//         .whereNotNull('image')
-//         .then(result => {
-//             console.log(result)
-//         })
-// }
+function getProductsWithImages() {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category', 'image')
+    .from('amazong_products')
+    .whereNotNull('image')
+    .then(result => {
+      console.log(result);
+    });
+}
 
 // getProductsWithImages()
 
-
-
-
-// function mostPopularVideosForDays(days) {
-//     knexInstance
-//         .select('video_name', 'region')
-//         .count('date_viewed AS views')
-//         .where(
-//             'date_viewed',
-//             '>',
-//             knexInstance.raw(`now() - '??days'::INTERVAL`, days)
-//         )
-//         .from('whopipe_video_views')
-//         .groupBy('video_name', 'region')
-//         .orderBy([
-//             { column: 'region', order: 'ASC'},
-//             { column: 'views', order: 'DESC'}
-//         ])
-//         .then(result => {
-//             console.log(result)
-//         })
-// }
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed',
+      '>',
+      knexInstance.raw(`now() - '??days'::INTERVAL`, days)
+    )
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC'},
+      { column: 'views', order: 'DESC'}
+    ])
+    .then(result => {
+      console.log(result);
+    });
+}
 
 // mostPopularVideosForDays(30)
