4cd452e19dedd721f2792ae2229fee95d4dcdf90
Ryan Vargas <i.am.ryan.v@gmail.com>
2020-06-10 10:45:50 -0700

    Implemented testing functionality

--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -1,7 +1,8 @@
+'use strict';
 const ShoppingListService = require('../src/shopping-list-service');
 const knex = require('knex');
 
-describe('Shopping List mockup', function() {
+describe('Shopping List mockup', function () {
   let db;
 
   before(() => {
@@ -11,6 +12,12 @@ describe('Shopping List mockup', function() {
     });
   });
 
+  before(() => db('shopping_list').truncate());
+
+  afterEach(() => db('shopping_list').truncate());
+
+  after(() => db.destroy());
+
   let listItems = [
     {
       id: 1,
@@ -18,7 +25,7 @@ describe('Shopping List mockup', function() {
       price: '12.22',
       date_added: Date.now(),
       checked: false,
-      category: 'Main'
+      category: 'Main',
     },
     {
       id: 2,
@@ -26,7 +33,7 @@ describe('Shopping List mockup', function() {
       price: '4.58',
       date_added: Date.now(),
       checked: false,
-      category: 'Snack'
+      category: 'Snack',
     },
     {
       id: 3,
@@ -34,7 +41,7 @@ describe('Shopping List mockup', function() {
       price: '8.14',
       date_added: Date.now(),
       checked: false,
-      category: 'Lunch'
+      category: 'Lunch',
     },
     {
       id: 4,
@@ -42,22 +49,110 @@ describe('Shopping List mockup', function() {
       price: '6.33',
       date_added: Date.now(),
       checked: false,
-      category: 'Breakfast'
-    }
+      category: 'Breakfast',
+    },
   ];
+
+  context(`Given 'shopping_list' has data`, () => {
+    beforeEach(() => {
+      return db.into('shopping_list').insert(testItems);
+    });
+
+    it(`getItems() fetches all items from 'shopping_list'`, () => {
+      const expectedItems = testItems.map((item) => ({
+        ...item,
+        checked: false,
+      }));
+      return ShoppingListService.getAllItems(db).then((actual) => {
+        expect(actual).to.eql(expectedItems);
+      });
+    });
+
+    it(`getById() fetches an item by id from 'shopping_list'`, () => {
+      const idToGet = 3;
+      const thirdItem = testItems[idToGet - 1];
+      return ShoppingListService.getById(db, idToGet).then((actual) => {
+        expect(actual).to.eql({
+          id: idToGet,
+          name: thirdItem.name,
+          date_added: thirdItem.date_added,
+          price: thirdItem.price,
+          category: thirdItem.category,
+          checked: false,
+        });
+      });
+    });
+
+    it(`deleteItem() finds an item by id and removes it from 'shopping_list'`, () => {
+      const idToDelete = 3;
+      return ShoppingListService.deleteItem(db, idToDelete)
+        .then(() => ShoppingListService.getAllItems(db))
+        .then((allItems) => {
+          // copy the test items array without the removed item
+          const expected = testItems
+            .filter((item) => item.id !== idToDelete)
+            .map((item) => ({
+              ...item,
+              checked: false,
+            }));
+          expect(allItems).to.eql(expected);
+        });
+    });
+
+    it(`updateItem() updates an item in 'shopping_list'`, () => {
+      const idOfItemToUpdate = 3;
+      const newItemData = {
+        name: 'new title',
+        price: '14.76',
+        date_added: Date.now(),
+        checked: true,
+      };
+      const originalItem = testItems[idOfItemToUpdate - 1];
+      return ShoppingListService.updateItem(db, idOfItemToUpdate, newItemData)
+        .then(() => ShoppingListService.getById(db, idOfItemToUpdate))
+        .then((item) => {
+          expect(item).to.eql({
+            id: idOfItemToUpdate,
+            ...originalItem,
+            ...newItemData,
+          });
+        });
+    });
+  });
+
+  context(`Given 'shopping_list' has no data`, () => {
+    it(`getItems() returns an empty array`, () => {
+      return ShoppingListService.getAllItems(db).then((actual) => {
+        expect(actual).to.eql([]);
+      });
+    });
+
+    it(`insertItem() inserts an item`, () => {
+      const newItem = {
+        name: 'New item',
+        price: '10.00',
+        date_added: Date.now(),
+        checked: true,
+        category: 'Lunch',
+      };
+      return ShoppingListService.insertItem(db, newItem).then((actual) => {
+        expect(actual).to.eql({
+          id: 1,
+          name: newItem.name,
+          price: newItem.price,
+          date_added: newItem.date_added,
+          checked: newItem.checked,
+          category: newItem.category,
+        });
+      });
+    });
+  });
 });
 
 // get
 
-
-
-
 // insert
 
-
-
 // update
-
-
 
 // delete
